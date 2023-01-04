73abc54c00391708d61896fd9d5b9625dd21b011
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-01-04 21:07:14 +0400

    Delete lesson_4_3.py

--- lesson_4_3.py
+++
@@ -1,5 +0,0 @@
-# 3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Решите задание в одну
-# строку.
-
-print(
-    f"Numbers that are multiples of 20 or 21 in the range from 20 to 240: {[i for i in range(20, 240) if i % 20 == 0 or i % 21 == 0]}")
