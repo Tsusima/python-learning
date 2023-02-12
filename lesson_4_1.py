5a38706c133106132ee85f57343598272db99eef
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-02-12 15:01:39 +0300

    Update lesson_4_1.py

--- lesson_4_1.py
+++ lesson_4_1.py
@@ -7,12 +7,15 @@ from sys import argv
 
 if len(argv) > 1:
     name_script, time_work, rate_per_hour, bonus = argv
-    time_work = int(time_work)
-    rate_per_hour = int(rate_per_hour)
-    bonus = int(bonus)
+    time_work = float(time_work)
+    rate_per_hour = float(rate_per_hour)
+    bonus = float(bonus)
     print((time_work * rate_per_hour) + bonus)
 else:
-    time_work = int(input("Enter time work: "))
-    rate_per_hour = int(input("Enter rate per hour: "))
-    bonus = int(input("Enter bonus: "))
-    print(f"salary: {(time_work * rate_per_hour) + bonus}")
+    try:
+        time_work = float(input("Enter time work: "))
+        rate_per_hour = float(input("Enter rate per hour: "))
+        bonus = float(input("Enter bonus: "))
+        print(f"salary: {(time_work * rate_per_hour) + bonus}")
+    except ValueError as e:
+        print(e)
