40b82ca6f7330001ab6312c86e676d2a8e8bc209
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-02-18 15:47:21 +0300

    Update lesson_4_7.py

--- lesson_4_7.py
+++ lesson_4_7.py
@@ -5,19 +5,22 @@
 # Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал
 # четырёх 4! = 1 * 2 * 3 * 4 = 24.
 
-from itertools import count
 from math import factorial
 
-def fibo_gen():
-    for el in count(1):
-        yield factorial(el)
 
-gen = fibo_gen()
-x = 0
-a = int(input("Enter the number up to which the fatcorial will be calculated: "))
-for i in gen:
-    if x < a:
-        print(i)
-        x += 1
-    else:
-        break
+def fact(n: int):
+    for i in range(1, n + 1):
+        yield factorial(i)
+
+
+if True:
+    try:
+        s = 0
+        value = int(input("Enter the number up to which the fatcorial will be calculated: "))
+    except ValueError as e:
+        print(e)
+        exit(1)
+
+    for el in fact(value):
+        s += 1
+        print(f"fatcorial {s} is {el}")
