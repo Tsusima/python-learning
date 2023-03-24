2f232186360d22d79dbefad06dd1abf4a488222b
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-03-24 20:34:55 +0300

    Update lesson_5_4.py

--- lesson_5_4.py
+++ lesson_5_4.py
@@ -8,13 +8,13 @@
 # числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
 
-with open("test_file_5_4.txt") as my_file:
-    data = my_file.read()
-en = ['One', 'Two', 'Three', 'Four']
-ru = ['Один', 'Два', 'Три', 'Четыре']
-for index, word in enumerate(en):
-    if word in data:
-        data = data.replace(word, ru[index])
-print(data)
-with open("test_new_file_5_4", "w") as my_file:
-    my_file.write(data)
+my_dict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open("test_file_5_4.txt", "r", encoding="utf-8") as my_file:
+    with open("test_new_file_5_4.txt", "w", encoding="utf-8") as new_file:
+        for line in my_file:
+            for key, value in my_dict.items():
+                line = line.replace(key, value)
+            print(line, end='')
+            words = line.split()
+            for word in words:
+                new_file.write(word + '\n')
