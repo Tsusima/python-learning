a3afd20e4765117929ec166bcee27010b53d8d4c
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-05-07 20:06:04 +0300

    Add files via upload

---
+++ lesson_6_3.py
@@ -0,0 +1,39 @@
+# 3. Реализовать базовый класс Worker (работник).
+# ● определить атрибуты: name, surname, position (должность), income (доход);
+# ● последний атрибут должен быть защищённым и ссылаться на словарь, содержащий
+# элементы: оклад и премия, например, {"wage": wage, "bonus": bonus};
+# ● создать класс Position (должность) на базе класса Worker;
+# ● в классе Position реализовать методы получения полного имени сотрудника
+# (get_full_name) и дохода с учётом премии (get_total_income);
+# ● проверить работу примера на реальных данных: создать экземпляры класса Position,
+# передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return print(f"Full name: {self.name} {self.surname}")
+
+    def get_total_income(self):
+        return print(f"Total income: {self._income.get('wage') + self._income.get('bonus')}")
+
+
+if __name__ == "__main__":
+    try:
+        name, surname, position, wage, bonus = input("Enter your name: "), input("Enter your surname: "), input(
+            "Enter your position: "), int(input("Enter your wage: ")), int(input("Enter your bonus: "))
+        pt = Position(name, surname, position, wage, bonus)
+        pt.get_full_name()
+        pt.get_total_income()
+    except ValueError as e:
+        print(e)
