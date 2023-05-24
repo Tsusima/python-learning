5817a025336006104e22cff443fba3377785cc7d
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-05-24 19:03:12 +0400

    Update lesson_6_3.py

--- lesson_6_3.py
+++ lesson_6_3.py
@@ -7,6 +7,7 @@
 # (get_full_name) и дохода с учётом премии (get_total_income);
 # ● проверить работу примера на реальных данных: создать экземпляры класса Position,
 # передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+
 class Worker:
 
     def __init__(self, name, surname, position, wage, bonus):
@@ -22,10 +23,11 @@ class Position(Worker):
         super().__init__(name, surname, position, wage, bonus)
 
     def get_full_name(self):
-        return print(f"Full name: {self.name} {self.surname}")
+        full_name = (f"{self.name} {self.surname}")
+        return full_name
 
     def get_total_income(self):
-        return print(f"Total income: {self._income.get('wage') + self._income.get('bonus')}")
+        return self._income.get('wage') + self._income.get('bonus')
 
 
 if __name__ == "__main__":
@@ -33,7 +35,41 @@ if __name__ == "__main__":
         name, surname, position, wage, bonus = input("Enter your name: "), input("Enter your surname: "), input(
             "Enter your position: "), int(input("Enter your wage: ")), int(input("Enter your bonus: "))
         pt = Position(name, surname, position, wage, bonus)
-        pt.get_full_name()
-        pt.get_total_income()
+        print(f"Full name: {pt.get_full_name()}")
+        print(f"Total income: {pt.get_total_income()}")
+
     except ValueError as e:
         print(e)
+
+        
+     
+# class Worker:
+
+#     def __init__(self, name, surname, position, wage, bonus):
+#         self.name = name
+#         self.surname = surname
+#         self.position = position
+#         self._income = {"wage": wage, "bonus": bonus}
+
+
+# class Position(Worker):
+
+#     def __init__(self, name, surname, position, wage, bonus):
+#         super().__init__(name, surname, position, wage, bonus)
+
+#     def get_full_name(self):
+#         return print(f"Full name: {self.name} {self.surname}")
+
+#     def get_total_income(self):
+#         return print(f"Total income: {self._income.get('wage') + self._income.get('bonus')}")
+
+
+# if __name__ == "__main__":
+#     try:
+#         name, surname, position, wage, bonus = input("Enter your name: "), input("Enter your surname: "), input(
+#             "Enter your position: "), int(input("Enter your wage: ")), int(input("Enter your bonus: "))
+#         pt = Position(name, surname, position, wage, bonus)
+#         pt.get_full_name()
+#         pt.get_total_income()
+#     except ValueError as e:
+#         print(e)
