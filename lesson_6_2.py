4b800876ab79f5aa4572c658382b08043e276e2e
Tsusima <115890187+Tsusima@users.noreply.github.com>
2023-05-24 18:52:46 +0400

    Update lesson_6_2.py

--- lesson_6_2.py
+++ lesson_6_2.py
@@ -7,21 +7,38 @@
 # дороги асфальтом, толщиной в 1 см*число см толщины полотна;
 # ● проверить работу метода.
 # Например: 20 м*5000 м*25 кг*5 см = 12500 т.
-
 class Road:
     def __init__(self, length, width):
-        self.length = length
-        self.width = width
+        self.__length = length
+        self.__width = width
 
     def mass_calculation(self):
-        mass = self.length * self.width * 25 * 5 / 1000
-        print(f'Need {mass} ton for the building')
+        mass = self.__length * self.__width * 25 * 5 / 1000
+        return mass
 
 
 if __name__ == '__main__':
     try:
         length, width = int(input("Enter length: ")), int(input("Enter width "))
         rd = Road(length, width)
-        rd.mass_calculation()
+        print(f'Need {rd.mass_calculation()} ton for the building')
     except ValueError as e:
         print(e)
+
+# class Road:
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+
+#     def mass_calculation(self):
+#         mass = self.length * self.width * 25 * 5 / 1000
+#         print(f'Need {mass} ton for the building')
+
+
+# if __name__ == '__main__':
+#     try:
+#         length, width = int(input("Enter length: ")), int(input("Enter width "))
+#         rd = Road(length, width)
+#         rd.mass_calculation()
+#     except ValueError as e:
+#         print(e)
